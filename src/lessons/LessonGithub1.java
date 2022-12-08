64822747db4b92937fa932e8529e8ca2d5d4dd8b
ZaferKok <zaferkok111@gmail.com>
2022-12-08 03:44:54 +0100

    ucuncu commit

--- src/lessons/LessonGithub1.java
+++ src/lessons/LessonGithub1.java
@@ -5,7 +5,10 @@ public class LessonGithub1 {
     public static void main(String[] args) {
 
         System.out.println("InteliJ'de github çalışması");
-
+        System.out.println("InteliJ'de github çalışması");
+        System.out.println("InteliJ'de github çalışması");
+        System.out.println("InteliJ'de github çalışması");
+        System.out.println("InteliJ'de github çalışması");
 
     }
 }
