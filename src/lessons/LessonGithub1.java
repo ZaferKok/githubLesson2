837860c64f979ae87e4cee58fcef001d9bc3c4af
ZaferKok <62161849+ZaferKok@users.noreply.github.com>
2022-12-08 03:46:03 +0100

    Update LessonGithub1.java

--- src/lessons/LessonGithub1.java
+++ src/lessons/LessonGithub1.java
@@ -6,9 +6,7 @@ public class LessonGithub1 {
 
         System.out.println("InteliJ'de github çalışması");
         System.out.println("InteliJ'de github çalışması");
-        System.out.println("InteliJ'de github çalışması");
-        System.out.println("InteliJ'de github çalışması");
-        System.out.println("InteliJ'de github çalışması");
+
 
     }
 }
