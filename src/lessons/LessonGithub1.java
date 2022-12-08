94c52dbd946f921b46cc9e32a7b1ad904670668b
ZaferKok <zaferkok111@gmail.com>
2022-12-08 03:43:44 +0100

    ikinci commit

--- src/lessons/LessonGithub1.java
+++ src/lessons/LessonGithub1.java
@@ -5,9 +5,12 @@ public class LessonGithub1 {
     public static void main(String[] args) {
 
         System.out.println("InteliJ'de github çalışması");
-                System.out.println("InteliJ'de github çalışması");
-
-                System.out.println("InteliJ'de github çalışması");
+        System.out.println("InteliJ'de github çalışması");
+        System.out.println("InteliJ'de github çalışması");
+        System.out.println("InteliJ'de github çalışması");
+        System.out.println("InteliJ'de github çalışması");
+        System.out.println("InteliJ'de github çalışması");
+        System.out.println("InteliJ'de github çalışması");
         System.out.println("InteliJ'de github çalışması");
 
     }
